051bb24b5c297b207d276d41e923a4b5c32a687b
Aleksandr Isaev <stik_mc@mail.ru>
2024-06-15 11:07:56 +0300

    chenged Main.class

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
 import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
+    private static char[] sizeMultipliers = {'B', 'K', 'M', 'G', 'T'};
     public static void main(String[] args) {
+        System.out.println(getSizeFromHumanReadable("10Mb"));
 
-        String folderPath = "C:\\Users\\Isaev\\Desktop\\TEST_PAGE";
+        String folderPath = "C:\\Users\\Isaev\\Desktop\\ForApplication";
         File file = new File(folderPath);
 
         long start = System.currentTimeMillis();
@@ -35,41 +38,37 @@ public class Main {
 
     //TODO: 24B, 234Kb, 36Mb, 34Gb, 42Tb
     public static String getHumanReadableSize(long size) {
-        double sum;
-        double number = size * 0.1;
-        StringBuilder value = new StringBuilder();
-        if (number > 0 && number <= 102.4) {
-            value.append(size).append("B");
-        } else if (number > 102.4 && number <= 104857.6) {
-            sum = (size / Math.pow(2, 10));
-            String result = String.format("%.3f", sum);
-            value.append(result).append("Kb");
-        } else if (number > 104857.6 && number <= 107374182.4) {
-            sum = (size / Math.pow(2, 20));
-            String result = String.format("%.3f", sum);
-            value.append(result).append("Mb");
-        } else if (number > 107374182.4 && number <= 109951162777.6) {
-            sum = (size / Math.pow(2, 30));
-            String result = String.format("%.3f", sum);
-            value.append(result).append("Gb");
-        } else if (number > 109951162777.6) {
-            sum = (size / Math.pow(2, 40));
-            String result = String.format("%.3f", sum);
-            value.append(result).append("Tb");
-        } else {
-            value.append(size);
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+            double value = size / Math.pow(1024,i);
+            if (value < 1024) {
+                return Math.round(value) + "" + sizeMultipliers[i] + (i > 0 ? "b" : "");
+            }
         }
-        return value.toString();
+        return "Very big!";
     }
 
     //TODO: 24B, 234Kb, 36Mb, 34Gb, 42Tb
     //   24B, 234K, 36M, 34G, 42T
     //   235K => 235 * 1024 =240640
-    public long getSizeFromHumanReadable(String size) {
-        String[] strings = size.split("");
-        for (String string : strings) {
+    public static long getSizeFromHumanReadable(String size) {
+        HashMap<Character, Integer> char2multiplier = getMultipliers();
+        char sizeFactor = size
+                .replaceAll("[0-9\\s+]+", "")
+                .charAt(0);
+        int multiplier = char2multiplier.get(sizeFactor);
+        long length = multiplier * Long.valueOf(
+                size.replaceAll("[^0-9]", ""));
+        return length;
+    }
 
+    private static HashMap<Character, Integer> getMultipliers() {
+        HashMap<Character, Integer> char2multiplier = new HashMap<>();
+        for (int i = 0; i < sizeMultipliers.length; i++) {
+            char2multiplier.put(
+                    sizeMultipliers[i],
+                    (int) Math.pow(1024, i)
+            );
         }
-        return 0;
+        return char2multiplier;
     }
 }
