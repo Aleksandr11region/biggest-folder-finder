4f0a6aa25f5cbf00f39191e5019e0467acf0797f
Aleksandr Isaev <stik_mc@mail.ru>
2024-06-15 10:02:30 +0300

    add getHumanReadableSize

--- src/FolderSizeCalculator.java
+++ src/FolderSizeCalculator.java
@@ -3,18 +3,15 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
-public class FolderSizeCalculator extends RecursiveTask<Long>
-{
+public class FolderSizeCalculator extends RecursiveTask<Long> {
     private File folder;
 
-    public FolderSizeCalculator(File folder)
-    {
+    public FolderSizeCalculator(File folder) {
         this.folder = folder;
     }
 
     @Override
-    protected Long compute()
-    {
+    protected Long compute() {
         if (folder.isFile()) {
             return folder.length();
         }
@@ -22,15 +19,13 @@ public class FolderSizeCalculator extends RecursiveTask<Long>
         long sum = 0;
         List<FolderSizeCalculator> subTasks = new LinkedList<>();
         File[] files = folder.listFiles();
-        for(File file : files)
-        {
+        for(File file : files) {
             FolderSizeCalculator task = new FolderSizeCalculator(file);
             task.fork(); //запускаем асинхронно
             subTasks.add(task);
         }
 
-        for(FolderSizeCalculator task : subTasks)
-        {
+        for(FolderSizeCalculator task : subTasks) {
             sum += task.join();
         }
 
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 import java.io.File;
 import java.util.concurrent.ForkJoinPool;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
 
-        String folderPath = "C:\\Users\\Isaev\\Desktop";
+        String folderPath = "C:\\Users\\Isaev\\Desktop\\TEST_PAGE";
         File file = new File(folderPath);
 
         long start = System.currentTimeMillis();
@@ -14,7 +12,7 @@ public class Main
         FolderSizeCalculator calculator = new FolderSizeCalculator(file);
         ForkJoinPool pool = new ForkJoinPool();
         long size = pool.invoke(calculator);
-        System.out.println(size);
+        System.out.println(getHumanReadableSize(size));
 
         long duration = (System.currentTimeMillis() - start);
         System.out.println(duration + " ms");
@@ -23,8 +21,7 @@ public class Main
 
     }
 
-    public static long getFolderSize(File folder)
-    {
+    public static long getFolderSize(File folder) {
         if (folder.isFile()) {
             return folder.length();
         }
@@ -34,5 +31,45 @@ public class Main
             sum += getFolderSize(file);
         }
         return sum;
+    }
+
+    //TODO: 24B, 234Kb, 36Mb, 34Gb, 42Tb
+    public static String getHumanReadableSize(long size) {
+        double sum;
+        double number = size * 0.1;
+        StringBuilder value = new StringBuilder();
+        if (number > 0 && number <= 102.4) {
+            value.append(size).append("B");
+        } else if (number > 102.4 && number <= 104857.6) {
+            sum = (size / Math.pow(2, 10));
+            String result = String.format("%.3f", sum);
+            value.append(result).append("Kb");
+        } else if (number > 104857.6 && number <= 107374182.4) {
+            sum = (size / Math.pow(2, 20));
+            String result = String.format("%.3f", sum);
+            value.append(result).append("Mb");
+        } else if (number > 107374182.4 && number <= 109951162777.6) {
+            sum = (size / Math.pow(2, 30));
+            String result = String.format("%.3f", sum);
+            value.append(result).append("Gb");
+        } else if (number > 109951162777.6) {
+            sum = (size / Math.pow(2, 40));
+            String result = String.format("%.3f", sum);
+            value.append(result).append("Tb");
+        } else {
+            value.append(size);
+        }
+        return value.toString();
+    }
+
+    //TODO: 24B, 234Kb, 36Mb, 34Gb, 42Tb
+    //   24B, 234K, 36M, 34G, 42T
+    //   235K => 235 * 1024 =240640
+    public long getSizeFromHumanReadable(String size) {
+        String[] strings = size.split("");
+        for (String string : strings) {
+
+        }
+        return 0;
     }
 }
